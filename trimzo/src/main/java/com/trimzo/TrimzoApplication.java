4195a7535a7115fb1833cb926459d07e9ea73260
Prince Kumar <114931770+im-prince@users.noreply.github.com>
2026-07-22 22:36:27 +0530

    project setup URL Sortner

---
+++ trimzo/src/main/java/com/trimzo/TrimzoApplication.java
@@ -0,0 +1,13 @@
+package com.trimzo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrimzoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrimzoApplication.class, args);
+	}
+
+}
---
+++ trimzo/src/test/java/com/trimzo/TrimzoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trimzo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrimzoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
